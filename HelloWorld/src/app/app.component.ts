b91e00112c7638e9cca896874f3a47b59db65e60
harshveer7828 <harshveer7828@gmail.com>
2025-03-05 21:51:57 +0530

    [Harsh veer singh] Add. Display bridge logo image✨

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'HelloWorld';
-
+  imgUrl = "BL_logo_square_jpg.jpg";
 
   ngOnInit():void{
     this.title = "Hello from BridgeLabz."
