96d2c8a11390b5a81f276fa73d240cdc15c0f3fb
harshveer7828 <harshveer7828@gmail.com>
2025-03-05 22:27:00 +0530

    [Harsh veer singh] Add. Ability to validate username at runtime and show error message if it is not valid✨

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
@@ -12,15 +12,27 @@ export class AppComponent {
   title = 'HelloWorld';
   imgUrl = 'BL_logo_square_jpg.jpg';
   url = 'https://www.bridgelabz.com';
-  username: String = '';
-
+  username: string = "";
+  nameError : string = "";
   ngOnInit(): void {
     this.title = 'Hello from BridgeLabz.';
   }
 
   // Catch the event
-  onClick($event: any) {
+  onClick($event:any) {
     console.log('Save button is clicked!', $event);
     window.open(this.url, '_blank');
+  }
+
+  // Check input event to check the username at real time
+  onInput($event:any){
+    console.log("Change Event Occurred!",$event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.username)){
+      this.nameError ="";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
+    
   }
 }
