59ce7da5a02c5d9c4578a91b62b2e4d49b6aa368
harshveer7828 <harshveer7828@gmail.com>
2025-03-05 22:14:15 +0530

    [Harsh veer singh] Add. Ability to take username and display message at real time✨

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,25 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'HelloWorld';
-  imgUrl = "BL_logo_square_jpg.jpg";
-  url = "https://www.bridgelabz.com";
+  imgUrl = 'BL_logo_square_jpg.jpg';
+  url = 'https://www.bridgelabz.com';
+  username: String = '';
 
-  ngOnInit():void{
-    this.title = "Hello from BridgeLabz."
+  ngOnInit(): void {
+    this.title = 'Hello from BridgeLabz.';
   }
 
   // Catch the event
-  onClick($event:any){
-    console.log("Save button is clicked!",$event);
-    window.open(this.url, "_blank")    
+  onClick($event: any) {
+    console.log('Save button is clicked!', $event);
+    window.open(this.url, '_blank');
   }
-
 }
