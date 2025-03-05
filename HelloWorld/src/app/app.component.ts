8cd6290cee1487bf9e83dafd00c73f8be3ff5044
harshveer7828 <harshveer7828@gmail.com>
2025-03-05 22:00:57 +0530

    [Harsh veer singh] Add. Ability added to launch on bridgelab site on clicking the logo✨

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -10,8 +10,16 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "BL_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com";
 
   ngOnInit():void{
     this.title = "Hello from BridgeLabz."
   }
+
+  // Catch the event
+  onClick($event:any){
+    console.log("Save button is clicked!",$event);
+    window.open(this.url, "_blank")    
+  }
+
 }
